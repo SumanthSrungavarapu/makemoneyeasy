9a99a2fee7f5ad7c578eed25d47b5c6f56539c35
SumanthSrungavarapu <sumanthsrungavarapu3@gmail.com>
2023-07-03 22:29:51 +0530

    tested tabula

--- extract.py
+++ extract.py
@@ -1,2 +1,11 @@
 #This code aims to extract data from a pdf
+# resources referred:
+#https://medium.com/analytics-vidhya/python-packages-for-pdf-data-extraction-d14ec30f0ad0
+#Considered packages(these maintain the structure, hopefully some maintain table structure too)
+# PDFtotext,PDFminer,Tabula
 
+#using Tabula
+import tabula as tabula
+path="statement.pdf"
+df = tabula.read_pdf(path, pages='all')
+print(df[0])
